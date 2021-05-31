826edb37308e5a8d0ba98a18e118b1c2cce0309d
JoyceRuobingZhang <joyceruobingzhang@gmail.com>
2021-05-30 22:23:28 -0500

    ternary solution

--- scripts/Metals.js
+++ scripts/Metals.js
@@ -5,11 +5,8 @@ const metals = getMetals()
 
 export const Metals = () => {
     let metalHtml = "<ul>"
-
-    // This is how you have been converting objects to <li> elements
-
-
-    // add conditions to determine which button was selected, then display html 
+        // This is how you have been converting objects to <li> elements
+        // add conditions to determine which button was selected, then display html 
 
     /* 🔴🔴🔴
         you need to determine which one should be checked by default - based on the current state of orderBuilder.
@@ -22,6 +19,7 @@ export const Metals = () => {
             return `<li><input type="radio" name="metal" value="${metal.id}"  /> ${metal.metal}</li>`
         })
         metalHtml += listedArr.join("")
+        metalHtml += "</ul>"
         return metalHtml
     } else if (orderBuilder.metalId) {
 
@@ -38,14 +36,25 @@ export const Metals = () => {
         let theItemIndex = listedArr.findIndex(item => {
             return item.match(`${selectedMetal.id}`)
         })
-        listedArr[theItemIndex] = `<li><input type="radio" name="metal" value="${selectedMetal.id}" checked /> ${selectedMetal.metal}</li>`
+        listedArr[theItemIndex] = `<li>
+        <input type="radio" name="metal" value="${selectedMetal.id}" checked /> ${selectedMetal.metal}
+        </li>`
 
         metalHtml += listedArr.join("")
+        metalHtml += "</ul>"
+
+        // // 🟡🟡🟡 simpler solution
+        // const selectedMetal = metals.find(metal => metal.id === orderBuilder.metalId)
+        // metalHtml += metals.map(metal =>
+        //     `<li>
+        //     <input type="radio" name="metal" value="${metal.id}" ${metal.id === selectedMetal.id ? 'checked ' : ''} />
+        //     ${selectedMetal.metal}</li>`)
+
         return metalHtml
     }
+    //🔴 when you return in a function, you reutrn to the next higher scope;
+    // 🔴But when you return in an if block, it will just jump out of the Metals (line 6) function.
 
-    metalHtml += "</ul>"
-    return metalHtml
 }
 
 
